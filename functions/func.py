5f41625c6f077610da9bcc426dfec517925cc719
R2SEEE <danil.burnosenko@icloud.com>
2024-03-05 18:32:22 +0700

    add recursia sin

--- functions/func.py
+++ functions/func.py
@@ -6,10 +6,16 @@ def sin_maclaurin(x, n):
     Функция синуса без автоматической
     подборки точности
     """
-    result = 0
-    for i in range(n):
-        result += ((-1) ** i) * (x ** (2 * i + 1)) / math.factorial(2 * i + 1)
-    return result
+    # result = 0
+    # for i in range(n):
+    #     result += ((-1) ** i) * (x ** (2 * i + 1)) / math.factorial(2 * i + 1)
+    # return result
+
+    if n == 0:
+        return 0
+    else:
+        term = ((-1) ** (n - 1)) * (x ** (2 * n - 1)) / math.factorial(2 * n - 1)
+        return term + sin_maclaurin(x, n - 1)
 
 
 def cos_maclaurin(x, n):
