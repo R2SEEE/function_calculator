92afaa1ecef060dd2fedc899cbbfac99a380d95d
IcavenPric <ivan.shtukaliuk@mail.ru>
2024-03-08 14:09:10 +0700

    add cos() and sin() functions

--- functions/func.py
+++ functions/func.py
@@ -1,32 +1,49 @@
 import math
 
 
-def sin_maclaurin(x, n):
+def evaluate_error(step, x):
+    return x ** step / math.factorial(step)
+
+
+def sin_maclaurin(x):
     """
     Функция синуса без автоматической
     подборки точности
     """
-    # result = 0
-    # for i in range(n):
-    #     result += ((-1) ** i) * (x ** (2 * i + 1)) / math.factorial(2 * i + 1)
-    # return result
 
-    if n == 0:
-        return 0
-    else:
-        term = ((-1) ** (n - 1)) * (x ** (2 * n - 1)) / math.factorial(2 * n - 1)
-        return term + sin_maclaurin(x, n - 1)
+    n = 0
+    result = 0
+    while True:
+        n += 1
+        if n <= 5:
+            result += ((-1) ** (n - 1)) * (x ** (2 * n - 1)) / math.factorial(2 * n - 1)
+            continue
+        elif evaluate_error(step=n, x=x) > pow(10, -9):
+            result += ((-1) ** (n - 1)) * (x ** (2 * n - 1)) / math.factorial(2 * n - 1)
+            continue
+        else:
+            break
+    return round(result, 9)
 
 
-def cos_maclaurin(x, n):
+def cos_maclaurin(x):
     """
     Функция косинуса без автоматической
     подборки точности
     """
-    result = 0
-    for i in range(n):
-        result += ((-1) ** i) * (x ** (2 * i)) / math.factorial(2 * i)
-    return result
+
+    result = 1
+    n = 1
+    while True:
+        n += 1
+        if n <= 5:
+            result += ((-1) ** (n - 1)) * (x ** (2 * n - 2)) / math.factorial(2 * n - 2)
+        elif evaluate_error(step=n, x=x) > pow(10, -9):
+            result += ((-1) ** (n - 1)) * (x ** (2 * n - 2)) / math.factorial(2 * n - 2)
+            continue
+        else:
+            break
+    return round(result, 9)
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import math
+
 from functions.func import sin_maclaurin, cos_maclaurin
 
 
 if "__main__" == __name__:
-    print(f"sin : {sin_maclaurin(3, 5)}")
-    print(f"cos : {cos_maclaurin(3, 5)}")
+    x = 3
+    print(f"sin({x}) : {sin_maclaurin(x)}")
+    print(f"cos({x}) : {cos_maclaurin(x)}")
+
