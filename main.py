7cc4e030caf625bdbcacc7e80ae408d424a2f695
IcavenPric <ivan.shtukaliuk@mail.ru>
2024-03-28 10:40:25 +0700

    exp, ln, hyper, binomial

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import math
 
-from functions.func import sin_maclaurin, cos_maclaurin
-
+from functions.func import sin_maclaurin, cos_maclaurin, exp_maclaurin, binomial_maclaurin, ln_maclaurin, \
+    hyperbole_maclaurin
 
 if "__main__" == __name__:
-    x = 3
+    x = 0.9
+    m = 10
     print(f"sin({x}) : {sin_maclaurin(x)}")
     print(f"cos({x}) : {cos_maclaurin(x)}")
-
+    print(f"exp({x}) : {exp_maclaurin(x)}")
+    print(f"(1 + {x}) ** {m} : {binomial_maclaurin(x, m)}")
+    print(f"ln(1 + {x}) : {ln_maclaurin(x)}")
+    print(f"1 / (1 + {x}) : {hyperbole_maclaurin(x)}")
